1affb0bdff40e148563ff5fbd31b67c6a34b4595
Jun Kang <jkang.alien@gmail.com>
2022-12-14 10:21:19 +0900

    tumor type level

--- R/dataExploration.R
+++ R/dataExploration.R
@@ -1,15 +1,25 @@
 library(tidyverse)
 
 mutation <- readr::read_csv(here::here("data/Pathology_NGS_mutation.csv"))
-clinical <- readr::read_csv(here::here("data/Pathology_NGS_1st.csv"))
+clinical <- readr::read_csv(here::here("data/Pathology_NGS_1st.csv"), na = "NULL")
 
 library(lubridate)
 
 clinicalDate <- clinical |>
-  mutate(prescription_date = ymd(prescription_date),
+  mutate(age = as.numeric(age),
+         prescription_date = ymd(prescription_date),
          year = lubridate::year(prescription_date),
          month = lubridate::month(prescription_date),
          year_month = floor_date(prescription_date, "1 month"))
+
+clinicalDate |>
+  ggplot(aes(x=age))+
+  geom_histogram()
+
+clinicalDate|>
+  group_by(year_month)|>
+  summarise(n=n(), meanAge = mean(age, na.rm = TRUE))
+
 
 clinicalDate |>
   ggplot2::ggplot(aes(x=year_month)) +
@@ -23,5 +33,14 @@ clinicalDate |>
   xlab("Year (Monthly)")+
   ylab("Number of NGS test \n (ThermoFisher)")+
   theme_minimal()
-  
+
+skimr::skim(clinicalDate)
+
+clinicalTumorType <- clinicalDate |>
+  mutate(tumor_type = as.factor(tumor_type))
+
+levels(clinicalTumorType$tumor_type)
+
+
+
 
